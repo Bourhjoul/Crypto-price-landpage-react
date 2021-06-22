1eaf5c3bfb06ff718e309f07956e70a37a4a8fa4
abdessamad bourhjoul <66276688+Bourhjoul@users.noreply.github.com>
2021-06-22 16:17:33 +0100

    main section and prices done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { ReactComponent as Sketch } from "./Sketch.svg";
+import { MdAddAlert } from "react-icons/all";
+import { useState } from "react";
 function App() {
+  const [coins, setcoins] = useState([
+    { name: "BTC", price: 49.158 },
+    { name: "ETH", price: 3.927 },
+    { name: "ADA", price: 345.27 },
+    { name: "DOGE", price: 54.7 },
+    { name: "XRP", price: 6.339 },
+  ]);
+
+  const [active, setactive] = useState(0);
   return (
     <div>
       <Navbar />
@@ -12,14 +23,43 @@ function App() {
           <div className="Form">
             <form>
               Send me an Email
-              <input type="email" className="forminput" placeholder="Email" />
+              <input
+                type="email"
+                className="forminput"
+                id="emailinput"
+                placeholder="Email"
+              />
               as soon as <br /> <br />
-              <select id="Select_Coin">
+              <select className="Select_Coin">
                 <option value="BTC">BTC</option>
                 <option value="ETH">ETH</option>
                 <option value="Doge">Doge</option>
                 <option value="XPR">XPR</option>
               </select>
+              Goes
+              <select className="Select_Coin">
+                <option value="Above">Above</option>
+                <option value="Below">Below</option>
+              </select>
+              The price of
+              <input
+                type="email"
+                className="forminput"
+                id="priceinput"
+                placeholder="0.00$"
+              />
+              <br />
+              <button type="submit" className="Set_Alert_Btn">
+                <MdAddAlert
+                  size="20"
+                  style={{
+                    verticalAlign: "middle",
+                    marginRight: "2px",
+                    marginBottom: "2px",
+                  }}
+                />{" "}
+                Set Alert
+              </button>
             </form>
           </div>
         </div>
@@ -27,6 +67,20 @@ function App() {
           <Sketch />
         </div>
       </section>
+      <div id="coins_prices">
+        {coins.map((coin, index) => (
+          <div
+            key={index}
+            className={`price_rect ${active === index && "activerect"}`}
+            onClick={() => setactive(index)}
+          >
+            {coin.name}
+            <p className={`price ${active === index && "activeprice"}`}>
+              {coin.price} $
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
